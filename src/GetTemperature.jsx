97cd6ad53d9348cf2c88c82bf60fcddc6e9da8f8
Modupetemi2k5 <151980601+Modupetemi2k5@users.noreply.github.com>
2024-04-21 07:50:13 -0700

    setting temp

--- src/GetTemperature.jsx
+++ src/GetTemperature.jsx
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function GetTemperature() {
-  return (
-    <div>
-      <span className="temp">{props.temp}</span>
-      <span className="deg">°</span>
-      <span className="cent">c</span>
-    </div>
-  );
+export default function GetTemperature(props) {
+  let [unit, setUnit] = useState("metric");
+  function showFahrhenite(event) {
+    event.preventDefault();
+    setUnit((props.temperature * 9) / 5 + 32);
+  }
+  function showCelcius(event) {
+    event.preventDefault();
+    setUnit(props.temperature);
+  }
+
+  if (unit === "metric") {
+    return (
+      <div>
+        <span className="temp">{props.temperature}</span>
+        <span className="deg">°</span>
+        <span className="cent">
+          C|
+          <a href="/" onClick={showFahrhenite}>
+            F
+          </a>
+        </span>
+        |
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <span className="temp">{props.temperature}</span>
+        <span className="deg">°</span>
+        <span className="cent">
+          <a href="/" onClick={showCelcius}>
+            {" "}
+            C
+          </a>
+          | F
+        </span>
+        |
+      </div>
+    );
+  }
 }
